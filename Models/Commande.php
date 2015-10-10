51832a825377d9097ff4c73f7f5e905be9ecd029
Sarah-mana <sarah.sangalli@greta-sud-aquitaine.fr>
2015-10-10 15:34:48 +0200

    Ajout des fichiers dans Models

---
+++ Models/Commande.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Commande
+ *
+ * @author Info
+ */
+class Commande {
+        private $id_commande = null;
+	private $c_dateretrait = null;
+	private $id_statut = null;
+	private $montantTva = null;
+	private $totalTTC_panier = null;
+	private $qteTotalArticles = null;
+	
+
+	public function __construct(){
+
+				
+	}
+        public function getId_commande() {
+            return $this->id_commande;
+        }
+
+        public function getC_dateretrait() {
+            return $this->c_dateretrait;
+        }
+
+        public function getId_statut() {
+            return $this->id_statut;
+        }
+
+        public function setId_commande($id_commande) {
+            $this->id_commande = $id_commande;
+        }
+
+        public function setC_dateretrait($c_dateretrait) {
+            $this->c_dateretrait = $c_dateretrait;
+        }
+
+        public function setId_statut($id_statut) {
+            $this->id_statut = $id_statut;
+        }
+}
